86ea6dc2e098ea603f6fbf8d87cc4c002fb9ca3d
sabine.hu <sabine.hu@epita.fr>
2021-11-14 12:48:21 +0100

    first commit

---
+++ Assets/ToModify/EdgeDetection.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class EdgeDetection : MonoBehaviour
+{
+    // Different filtering modes
+    public enum EdgeDetectionMode
+    {
+        Depth = 0,
+        Color = 1,
+        Normal = 2,
+        Custom = 3
+    }
+
+    // Boolean that triggers or disables the effect
+    public bool enableFilter = false;
+
+    // Detection mode for the edges
+    public EdgeDetectionMode detectionMode = EdgeDetectionMode.Color;
+
+    // Do not modify (compute shader used for the modification)
+    public ComputeShader edgeDetection = null;
+
+    void OnPreRender()
+    {
+        Camera cam = GetComponent<Camera>();
+        if (enableFilter && edgeDetection != null)
+            cam.depthTextureMode = DepthTextureMode.DepthNormals;
+        else
+            cam.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (enableFilter && edgeDetection != null)
+        {
+            // Request the render texture we will be needing
+            RenderTextureDescriptor rtD = new RenderTextureDescriptor();
+            rtD.width = src.width;
+            rtD.height = src.height;
+            rtD.volumeDepth = 1;
+            rtD.msaaSamples = 1;
+            rtD.dimension = UnityEngine.Rendering.TextureDimension.Tex2D;
+            rtD.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R16G16B16A16_SFloat;
+            rtD.enableRandomWrite = true;
+            RenderTexture edgeBuffer = RenderTexture.GetTemporary(rtD);
+
+            // Run the edge detection kernel
+            int tileX = (src.width + 7) / 8;
+            int tileY = (src.height + 7) / 8;
+            int kernelID = (int)detectionMode;
+            edgeDetection.SetTexture(kernelID, "_CameraColorBuffer", src);
+            edgeDetection.SetTexture(kernelID, "_EdgesBufferRW", edgeBuffer);
+            edgeDetection.Dispatch(kernelID, tileX, tileY, 1);
+
+            // Copy into the Screen
+            Graphics.Blit(edgeBuffer, dest);
+        }
+        else
+        {
+            // Copy into the Screen
+            Graphics.Blit(src, dest);
+        }
+    }
+}
